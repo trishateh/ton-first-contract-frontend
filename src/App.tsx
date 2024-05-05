c9efc5cd6bd16a4b4431fa4c6c41198a045b6531
trishateh <trishateh@gmail.com>
2024-05-06 00:04:24 +0800

    fix layout

--- src/App.tsx
+++ src/App.tsx
@@ -29,6 +29,7 @@ function App() {
       <div>
         <div className="Card">
           <b>{WebApp.platform}</b>
+          <br />
           <b>Our Contract Address</b>
           <div className="Hint">{contract_address?.slice(0, 30) + "..."}</div>
           <b>Our Contract Balance</b>
